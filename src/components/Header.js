d92c91e95d7c652ff3134d7ff224643fdbf1f529
Mateus de Medeiros Ramos <mateusdmramos@gmail.com>
2022-11-09 17:19:45 -0300

    criada página de Game e componente header para renderizar infos do player

---
+++ src/components/Header.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import md5 from 'crypto-js/md5';
+
+class Header extends Component {
+  state = {
+    linkImagem: '',
+  };
+
+  componentDidMount() {
+    const { email } = this.props;
+    const hashGerada = md5(email).toString();
+    const linkImagem = `https://www.gravatar.com/avatar/${hashGerada}`;
+    this.setState({ linkImagem });
+  }
+
+  render() {
+    const { linkImagem } = this.state;
+    const { name } = this.props;
+    return (
+      <header>
+        <img src={ linkImagem } alt="Imagem do jogador" />
+        <p data-testid="header-player-name">{ name }</p>
+        <p data-testid="header-score">0</p>
+      </header>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  name: state.player.name,
+  email: state.player.gravatarEmail,
+});
+
+Header.propTypes = {
+  name: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(Header);
