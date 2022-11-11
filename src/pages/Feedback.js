b915c3f20eda43b42abab209bbf19ba46ba794bc
als-menezes <als.menezes2@gmail.com>
2022-11-11 11:16:42 -0300

    requisito 13 done

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 
 class Feedback extends React.Component {
   render() {
+    const { assertions } = this.props;
+    const MIN_ANSWERS = 3;
     return (
-      <Header />
+      <>
+        <Header />
+        { assertions < MIN_ANSWERS
+          ? <p data-testid="feedback-text">Could be better...</p>
+          : <p data-testid="feedback-text">Well Done!</p>}
+      </>
     );
   }
 }
 
-export default Feedback;
+const mapStateToProps = (state) => ({
+  assertions: state.player.assertions,
+});
+
+Feedback.propTypes = {
+  assertions: PropTypes.number,
+}.isRequired;
+
+export default connect(mapStateToProps)(Feedback);
