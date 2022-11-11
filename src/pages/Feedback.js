48287a111be9977cbd2a312ebfe03ca04e25ac5c
als-menezes <als.menezes2@gmail.com>
2022-11-11 14:36:41 -0300

    requisito 14 done

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -5,11 +5,13 @@ import Header from '../components/Header';
 
 class Feedback extends React.Component {
   render() {
-    const { asserts } = this.props;
+    const { asserts, points } = this.props;
     const MIN_ANSWERS = 3;
     return (
       <>
         <Header />
+        <h3 data-testid="feedback-total-question">{ asserts }</h3>
+        <h3 data-testid="feedback-total-score">{ points }</h3>
         { asserts < MIN_ANSWERS
           ? <p data-testid="feedback-text">Could be better...</p>
           : <p data-testid="feedback-text">Well Done!</p>}
@@ -20,10 +22,12 @@ class Feedback extends React.Component {
 
 const mapStateToProps = (state) => ({
   asserts: state.player.assertions,
+  points: state.player.score,
 });
 
 Feedback.propTypes = {
   asserts: PropTypes.number,
+  points: PropTypes.number,
 }.isRequired;
 
 export default connect(mapStateToProps)(Feedback);
